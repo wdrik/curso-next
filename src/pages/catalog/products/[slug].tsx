929298df21a287dc2a92c1a7d76d6cb5b5096d4c
wdrik <w.drik@outlook.com>
2021-02-19 15:33:18 -0300

    first commit

---
+++ src/pages/catalog/products/[slug].tsx
@@ -0,0 +1,8 @@
+import { useRouter } from 'next/router'
+
+export default function Search() {
+  const router = useRouter()
+  return (
+    <h1>{router.query.slug}</h1>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { Title } from '../styles/pages/Home';
+
+export default function Home() {
+  return (
+    <div><Title>Hello World!</Title></div>
+  )
+}
