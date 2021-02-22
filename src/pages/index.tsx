f9e5858dca54b53f3327f5b4f61d69490f0aea72
wdrik <w.drik@outlook.com>
2021-02-22 11:09:10 -0300

    feat: first api call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from 'react';
 import { Title } from '../styles/pages/Home';
 
+interface IProduct {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+  const [recomendedProducts, setRecomendedProducts] = useState<IProduct[]>([]);
+
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response => {
+      response.json().then(data => {
+        setRecomendedProducts(data);
+      })
+    })
+  }, [])
+
   return (
-    <div><Title>Hello World!</Title></div>
+    <div>
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recomendedProducts.map(recomendedProduct => {
+            return (
+              <li key={recomendedProduct.id}>
+                {recomendedProduct.title}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+    </div>
   )
 }
